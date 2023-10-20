f8ee82413661d3ece5b13228637e014db0d4185e
leandrocgsi <leandrocgsi@gmail.com>
2023-10-20 16:52:30 -0300

    Initial Commit

--- src/main/java/br/com/erudio/service/ChatGptService.java
+++ src/main/java/br/com/erudio/service/ChatGptService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.service;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import br.com.erudio.vo.response.ChatGptResponse;
 
 @Service
 public class ChatGptService {
+    
+    private Logger logger = Logger.getLogger(ChatGptService.class.getName());
 
     @Value("${openai.model}")
     private String model;
@@ -21,8 +25,11 @@ public class ChatGptService {
     private RestTemplate template;
 
     public String chat(String prompt){
+        logger.info("Starting Prompt");
         ChatGptRequest request = new ChatGptRequest(model, prompt);
         ChatGptResponse response = template.postForObject(apiURL, request, ChatGptResponse.class);
+        
+        logger.info("Proccessing Response");
         return response.getChoices().get(0).getMessage().getContent();
     }
 }

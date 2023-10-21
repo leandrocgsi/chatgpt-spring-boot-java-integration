8b2a3a30986bc1c9d6e0e532f0578b35a5db62fb
leandrocgsi <leandrocgsi@gmail.com>
2023-10-21 20:06:05 -0300

    Initial Commit

--- src/main/java/br/com/erudio/config/OpenAIConfig.java
+++ src/main/java/br/com/erudio/config/OpenAIConfig.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -20,6 +21,7 @@ public class OpenAIConfig {
         logger.info("Initializing RestTemplate");
 
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
